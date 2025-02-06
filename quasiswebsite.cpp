fb36c6660836a2f1ea09b1af0c4a89fe752987b4
Quasimoto <167645959+LordQuazii@users.noreply.github.com>
2025-02-06 09:14:07 -0500

    Add files via upload

---
+++ quasiswebsite.cpp
@@ -0,0 +1,10 @@
+<html>
+<p> WELCOME TO MY WEBSITE! <p>
+<p> This is a beta version (really basic HTML only because Im lazy), it will look nice soon. <p>
+<p>Hello, I am Quasimoto, I make maps and maybe music (if I ever get around to recording anything)<P>
+<p> QUASI'S MUSIC LIST!<p>
+<a href="https://youtube.com/playlist?list=PL228FAD202E1D8335&si=7pD_qtBw_mBDHs-D">Exmilitary (2011) - Death Grips</a>
+<p> <a href="https://youtube.com/playlist?list=PLDUEjoBVMbh-99HhIIFev5YvQCnmerjkR&si=Agj8xQBmjzrufmLE">Are You Experienced? (1967) - The Jimi Hendrix Experience</a>
+<p> <a href="https://youtube.com/playlist?list=OLAK5uy_muAZdTmhO8pQ0vkGnXFJL0U5UlHZL1Jbc&si=ZB_wazgTYzrDHW_v">Milo Goes to College (1982) - The Descendents</a>
+<p>Goldsrc/Quake/Source Maps: (None yet!) <p>
+<p>Music: Also none :((<p>
